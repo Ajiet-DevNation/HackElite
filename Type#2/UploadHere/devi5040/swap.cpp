869a0238725c0b06de0ded4b4b54dd4e856cddb9
devi5040 <dpraidola@gmail.com>
2023-10-15 19:10:14 +0530

    swapping in cpp

---
+++ Type#2/UploadHere/devi5040/swap.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 10, b = 20;
+    cout << "Before swap a=" << a << " b=" << b << endl;
+    swap(a, b);
+    cout << "After swap a=" << a << " b=" << b << endl;
+}
