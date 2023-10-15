886963a1f83f01ca619e6461753da7a36eea70d8
Deviprasad Rai P <91744010+devi5040@users.noreply.github.com>
2023-10-15 19:11:51 +0530

    Revert "swapping in cpp"

--- Type#2/UploadHere/devi5040/swap.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    int a = 10, b = 20;
-    cout << "Before swap a=" << a << " b=" << b << endl;
-    swap(a, b);
-    cout << "After swap a=" << a << " b=" << b << endl;
-}
